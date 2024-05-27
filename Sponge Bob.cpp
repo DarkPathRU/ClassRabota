0b817e00aa7c0fc2b532c0c7b0ce32ac98eea788
ernest <ernestmuaythai96@gmail.com>
2024-05-27 15:39:55 +0300

    v1.0

---
+++ Sponge Bob.cpp
@@ -0,0 +1,54 @@
+﻿//#include <iostream>
+//#include <vector>
+//#include <optional>
+//#include <string>
+//#include <math.h>
+//#include <memory>
+//#include <tuple>
+//#include <forward_list>
+//#include <algorithm>
+//#include <string>
+//#include <numeric>
+//#include <regex>
+//#include <fstream>
+//#include<set>
+//#include<unordered_set>
+//#include<sstream>
+//#include<map>
+//using namespace std;
+
+
+
+//
+//
+//
+//															//dz1
+//set<string>getSet(string& str) {
+//	set<string> nums;
+//	stringstream newNums(str);
+//	while (getline(newNums, str, ' '));
+//	if (str.size() > 0) {
+//		nums.insert(str);
+//	}
+//	return nums;
+//}
+//
+//
+//	
+//int main()
+//{
+//	int n = 1;
+//	string test1;
+//	cout << "vvedite stroky 4isel: " << endl;
+//	getline(cin, test1); 
+//	set<char> setNum(test1.begin(), test1.end());
+//		if (test1.size() > setNum.size()) {
+//			cout << "v stroke est odnikaovie 4isla" << endl;
+//		}
+//		else {
+//			cout << "v stoke net odinakovih 4isel" << endl;
+//		}
+//	return 0;
+//
+//}
+//
